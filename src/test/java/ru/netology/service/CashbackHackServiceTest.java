59925771b80f902b4f908b625d960b9c56aaa2dc
Каймакчи Александр <kaymakchiao@mail.ru>
2022-07-20 16:29:15 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+   @Test
+    void cashbackHackServicePositiveTest() {
+ int amount = 1000;
+ int expected = 1000;
+ int actual = service.remain(amount);
+ assertEquals(actual,expected);
+   }
+    @Test
+    void cashbackHackServiceNegativeTest() {
+        int amount = 2000;
+        int expected = 2000;
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+}
