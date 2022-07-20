a097fc6149d03b94485bbd07d28deb2da292eb6d
Каймакчи Александр <kaymakchiao@mail.ru>
2022-07-20 22:30:57 +0300

    Forth commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,16 +12,16 @@ public class CashbackHackServiceTest {
 
     @Test
     public void cashbackHackServicePositiveTest() {
-        int amount = 1000;
-        int expected = 1000;
+        int amount = 800;
+        int expected = 200;
         int actual = service.remain(amount);
         assertEquals(actual, expected);
     }
 
     @Test
     public void cashbackHackServiceNegativeTest() {
-        int amount = 2000;
-        int expected = 2000;
+        int amount = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
         assertEquals(actual, expected);
     }
