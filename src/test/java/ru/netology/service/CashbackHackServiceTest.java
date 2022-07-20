e7de0c2c1a654b86437336c50084656c6707dcec
Каймакчи Александр <kaymakchiao@mail.ru>
2022-07-20 17:05:56 +0300

    Second commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,24 +1,26 @@
 package ru.netology.service;
 
+
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-   @Test
-    void cashbackHackServicePositiveTest() {
- int amount = 1000;
- int expected = 1000;
- int actual = service.remain(amount);
- assertEquals(actual,expected);
-   }
     @Test
-    public  void cashbackHackServiceNegativeTest() {
+    public void cashbackHackServicePositiveTest() {
+        int amount = 1000;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void cashbackHackServiceNegativeTest() {
         int amount = 2000;
         int expected = 2000;
         int actual = service.remain(amount);
-        assertEquals(actual,expected);
+        assertEquals(actual, expected);
     }
 }
